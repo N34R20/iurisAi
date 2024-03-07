8434ad4cc54c698d745eb179a13493a9303ae22e
N34R20 <francisco.olmos.99@gmail.com>
2024-03-07 19:47:57 -0300

    update PDFtoTEXT and sketch de grafos

--- PDFtoTXT.py
+++ PDFtoTXT.py
@@ -2,15 +2,35 @@
 pdf_file_path = 'FALLO CSS 081244_2015_CS001.pdf'
 txt_file_path = 'fallo.txt'
 
+import argparse
 
-# Abre el archivo PDF en modo lectura binaria
-with open(pdf_file_path, 'rb') as pdf_file:
-    # Lee todo el contenido binario del archivo
-    pdf_content = pdf_file.read()
+class SharedState:
+    def __init__(self):
+        pdf_path = None
 
-    # Decodifica el contenido binario a una cadena Unicode
-    #pdf_text = pdf_content.decode('utf-8')
+shared_state = SharedState()
 
-    # Imprime o procesa pdf_text según tus necesidades
-    print(pdf_content)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Set path for the pdf and .txt file.')
+    parser.add_argument('--pdf_path', type=str, help='Path to the pdf file')
+    return parser.parse_args()
 
+args = parse_arguments()
+
+shared_state.pdf_path = args.pdf_path
+
+print(shared_state.pdf_path)
+
+from PyPDF2 import PdfReader
+
+reader = PdfReader(pdf_file_path)
+len_pages = reader.getNumPages()
+
+# Inicializar una cadena para almacenar el texto extraído
+text = ''
+
+for page_num in range(len_pages):
+    page = reader.pages[page_num]
+    text += page.extract_text()
+
+print(text)
